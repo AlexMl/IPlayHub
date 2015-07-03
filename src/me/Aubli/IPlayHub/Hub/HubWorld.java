2a2ed64e77e12c17ee2a754297f1132e33cd79f6
AlexMl <alexandermlinzk@gmail.com>
2015-07-03 21:33:07 +0200

    Override toString, load constructor

--- src/me/Aubli/IPlayHub/Hub/HubWorld.java
+++ src/me/Aubli/IPlayHub/Hub/HubWorld.java
@@ -3,6 +3,7 @@ package me.Aubli.IPlayHub.Hub;
 import java.util.List;
 
 import org.bukkit.Location;
+import org.bukkit.configuration.ConfigurationSection;
 
 
 public class HubWorld {
@@ -15,7 +16,18 @@ public class HubWorld {
     public HubWorld(HubConfig config, Location spawnLoc) {
 	this.config = config;
 	this.hubSpawn = spawnLoc.clone();
+	// save
+    }
+    
+    public HubWorld(ConfigurationSection section) throws Exception {
+	this.config = new HubConfig(section);
+	// load
     }
     
     // TODO setter/getter & save/load
+    
+    @Override
+    public String toString() {
+	return getClass().getSimpleName() + "[World=" + this.config.getWorld().getName() + "]";
+    }
 }
