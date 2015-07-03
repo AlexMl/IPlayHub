1e7c2b3fc3e6f0fee9b2c088b4b02582eb74b112
AlexMl <alexandermlinzk@gmail.com>
2015-07-03 19:36:55 +0200

    Added custom Plugin logger

--- src/me/Aubli/IPlayHub/IPlayHub.java
+++ src/me/Aubli/IPlayHub/IPlayHub.java
@@ -1,21 +1,34 @@
 package me.Aubli.IPlayHub;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.util.Logger.PluginOutput;
 
 
 public class IPlayHub extends JavaPlugin {
     
     private static IPlayHub instance;
     
+    private static PluginOutput logger;
+    
     @Override
     public void onDisable() {
+	logger.log(getClass(), "Plugin disabled!", false);
     }
     
     @Override
     public void onEnable() {
+	instance = this;
+	
+	logger = new PluginOutput(getHub(), true, 100); // Missing config
+	
+	logger.log(getClass(), "Plugin enabled!", false);
     }
     
     public static IPlayHub getHub() {
 	return instance;
+    }
+    
+    public static PluginOutput getPluginLogger() {
+	return logger;
     }
 }
