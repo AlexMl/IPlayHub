63f46be64c9cc991ff164ac69b43038e08b84ac7
AlexMl <alexandermlinzk@gmail.com>
2015-07-03 21:49:54 +0200

    Added save/load for hubworld

--- src/me/Aubli/IPlayHub/Hub/HubWorld.java
+++ src/me/Aubli/IPlayHub/Hub/HubWorld.java
@@ -1,9 +1,15 @@
 package me.Aubli.IPlayHub.Hub;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import me.Aubli.IPlayHub.IPlayHub;
 
 import org.bukkit.Location;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 
 public class HubWorld {
@@ -16,15 +22,67 @@ public class HubWorld {
     public HubWorld(HubConfig config, Location spawnLoc) {
 	this.config = config;
 	this.hubSpawn = spawnLoc.clone();
-	// save
+	saveConfig();
     }
     
     public HubWorld(ConfigurationSection section) throws Exception {
 	this.config = new HubConfig(section);
-	// load
+	loadConfig();
     }
     
-    // TODO setter/getter & save/load
+    private void loadConfig() {
+	ConfigurationSection config = getConfig().getConfigSection();
+	
+	String[] locationString = config.getString("location.spawn").split(",");
+	this.hubSpawn = new Location(getConfig().getWorld(), Integer.parseInt(locationString[0]), Integer.parseInt(locationString[1]), Integer.parseInt(locationString[2]));
+	
+	this.teleportLocations = new ArrayList<Location>();
+	
+	for (String listEntry : config.getStringList("location.teleport")) {
+	    String[] locationListString = listEntry.split(",");
+	    this.teleportLocations.add(new Location(getConfig().getWorld(), Integer.parseInt(locationListString[0]), Integer.parseInt(locationListString[1]), Integer.parseInt(locationListString[2])));
+	}
+    }
+    
+    public void saveConfig() {
+	FileConfiguration configConfiguration = YamlConfiguration.loadConfiguration(IPlayHub.getHub().getWorldFile());
+	ConfigurationSection configSection = configConfiguration.getConfigurationSection("worlds." + getConfig().getWorld().getName());
+	configSection.set("location.spawn", getLocationString(getSpawnLocation()));
+	
+	List<String> locationStringList = new ArrayList<String>();
+	
+	for (Location loc : getTeleportPoints()) {
+	    locationStringList.add(getLocationString(loc));
+	}
+	configSection.set("location.teleport", locationStringList);
+	
+	try {
+	    configConfiguration.save(IPlayHub.getHub().getWorldFile());
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    // TODO log
+	}
+    }
+    
+    private String getLocationString(Location loc) {
+	return loc.getBlockX() + "," + loc.getBlockY() + "," + loc.getBlockZ();
+    }
+    
+    public Location getSpawnLocation() {
+	return this.hubSpawn.clone();
+    }
+    
+    public List<Location> getTeleportPoints() {
+	return this.teleportLocations;
+    }
+    
+    public HubConfig getConfig() {
+	return this.config;
+    }
+    
+    public boolean addTeleportPoint(Location location) {
+	return this.teleportLocations.add(location);
+    }
     
     @Override
     public String toString() {
