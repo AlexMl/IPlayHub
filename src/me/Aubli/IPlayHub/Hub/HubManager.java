6748a2299954d6f57ea7a40548c408be255d31ad
AlexMl <alexandermlinzk@gmail.com>
2015-07-03 20:55:29 +0200

    laod existing hubs and register methods

--- src/me/Aubli/IPlayHub/Hub/HubManager.java
+++ src/me/Aubli/IPlayHub/Hub/HubManager.java
@@ -1,8 +1,16 @@
 package me.Aubli.IPlayHub.Hub;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import me.Aubli.IPlayHub.IPlayHub;
+
+import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 
 public class HubManager {
@@ -17,10 +25,51 @@ public class HubManager {
     }
     
     private void initialize() {
+	FileConfiguration config = YamlConfiguration.loadConfiguration(IPlayHub.getHub().getWorldFile());
+	
+	this.hubList = new ArrayList<HubWorld>();
+	
+	if (config.get("worlds") != null) {
+	    
+	    for (String worlds : config.getConfigurationSection("worlds").getKeys(false)) {
+		System.out.println(worlds);
+	    }
+	    
+	}
+	
+	try {
+	    config.save(IPlayHub.getHub().getWorldFile());
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    // TODO logger
+	}
 	
     }
     
-    public HubWorld getHub(World world) {
+    public HubWorld registerHub(World world, Location spawnLocation) throws Exception {
+	HubWorld hub = new HubWorld(new HubConfig(world, createSection(world)), spawnLocation.clone());
+	this.hubList.add(hub);
+	return hub;
+    }
+    
+    private ConfigurationSection createSection(World world) throws Exception {
+	FileConfiguration config = YamlConfiguration.loadConfiguration(IPlayHub.getHub().getWorldFile());
 	
+	if (config.get("worlds." + world.getName()) == null) {
+	    config.set("worlds." + world.getName(), null);
+	    config.save(IPlayHub.getHub().getWorldFile());
+	    return config.getConfigurationSection("worlds." + world.getName());
+	} else {
+	    // TODO logger
+	    throw new Exception();
+	}
+    }
+    
+    public HubManager getManager() {
+	return instance;
+    }
+    
+    public HubWorld getHub(World world) {
+	return null;
     }
 }
