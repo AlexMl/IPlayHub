9380e22ec16cdaa0994208d19eb715f8fb51f120
AlexMl <alexandermlinzk@gmail.com>
2015-07-12 18:02:28 +0200

    Added Interact listener

--- src/me/Aubli/IPlayHub/Listener/SignListener.java
+++ src/me/Aubli/IPlayHub/Listener/SignListener.java
@@ -5,10 +5,14 @@ import me.Aubli.IPlayHub.Hub.HubManager;
 import me.Aubli.IPlayHub.Hub.WorldHub;
 
 import org.bukkit.Bukkit;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 
 
 public class SignListener implements Listener {
@@ -26,7 +30,10 @@ public class SignListener implements Listener {
 		    
 		    if (hub != null) {
 			if (hub.getTeleportPoint(tpName) != null) {
-			    // TODO format sign
+			    event.setLine(0, "[IPH]");
+			    event.setLine(1, "Hub Teleport");
+			    event.setLine(2, tpName);
+			    event.setLine(3, worldName);
 			    // Message
 			} else {
 			    // Message
@@ -38,9 +45,50 @@ public class SignListener implements Listener {
 		    // Message
 		}
 	    } else {
-		// Message
+		IPlayHubPermissions.deny(eventPlayer);
 		event.setCancelled(true);
 		return;
+	    }
+	}
+	
+    }
+    
+    @EventHandler
+    public void onSignInteract(PlayerInteractEvent event) {
+	
+	Player eventPlayer = event.getPlayer();
+	
+	if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+	    if (event.getClickedBlock().getState() instanceof Sign) {
+		Sign eventSign = (Sign) event.getClickedBlock().getState();
+		
+		if (eventSign.getLine(0).equalsIgnoreCase("[iph]")) { // TODO prefix
+		    event.setCancelled(true);
+		    
+		    if (eventPlayer.hasPermission(IPlayHubPermissions.Teleport.getPermissionNode())) {
+			String worldName = eventSign.getLine(3);
+			String tpName = eventSign.getLine(2);
+			WorldHub hub = HubManager.getManager().getHub(Bukkit.getWorld(worldName));
+			
+			if (hub != null) {
+			    if (hub.getTeleportPoint(tpName) != null) {
+				if (hub.isEnabled()) {
+				    eventPlayer.teleport(hub.getTeleportPoint(tpName).getLocation(), TeleportCause.PLUGIN);
+				    return;
+				} else {
+				    // Message
+				}
+			    } else {
+				// Message
+			    }
+			} else {
+			    // Message
+			}
+		    } else {
+			IPlayHubPermissions.deny(eventPlayer);
+			return;
+		    }
+		}
 	    }
 	}
 	
