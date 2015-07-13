baa02e810dbc3cc9ed2a5f58b29afaf60690df54
AlexMl <alexandermlinzk@gmail.com>
2015-07-13 11:45:00 +0200

    Added welcome message

--- src/me/Aubli/IPlayHub/Listener/ConnectionListener.java
+++ src/me/Aubli/IPlayHub/Listener/ConnectionListener.java
@@ -1,15 +1,32 @@
 package me.Aubli.IPlayHub.Listener;
 
+import me.Aubli.IPlayHub.IPlayHub;
+import me.Aubli.IPlayHub.Hub.HubManager;
+import me.Aubli.IPlayHub.Hub.WorldHub;
+
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 
 
 public class ConnectionListener implements Listener {
     
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH)
     public void onPlayerJoin(PlayerJoinEvent event) {
+	Player eventPlayer = event.getPlayer();
 	
+	if (IPlayHub.isJoinAtHub()) {
+	    WorldHub hub = HubManager.getManager().getHub(IPlayHub.getMainWorld());
+	    
+	    if (hub != null) {
+		eventPlayer.teleport(hub.getSpawnPoint().getLocation(), TeleportCause.PLUGIN);
+	    } else {
+		// TODO log
+	    }
+	}
+	eventPlayer.sendMessage(IPlayHub.getPluginPrefix() + " " + IPlayHub.getWelcomeMessage());
     }
-    
 }
