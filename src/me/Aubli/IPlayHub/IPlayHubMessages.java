e72daaa3069f0b7f1c9baad4a05c2a9520d81953
AlexMl <alexandermlinzk@gmail.com>
2015-07-13 14:13:45 +0200

    Added more messages

--- src/me/Aubli/IPlayHub/IPlayHubMessages.java
+++ src/me/Aubli/IPlayHub/IPlayHubMessages.java
@@ -7,9 +7,10 @@ import org.bukkit.entity.Player;
 public enum IPlayHubMessages {
     
     no_permission(ChatColor.DARK_RED + "You do not have enough Permissions for that!"),
-    no_hub_in_world(ChatColor.RED + "There is no Hub in " + ChatColor.GOLD + "%s" + ChatColor.RED + " !"),
+    no_hub_in_world(ChatColor.RED + "There is no Hub available in " + ChatColor.GOLD + "%s" + ChatColor.RED + " !"),
     no_teleport_points(ChatColor.RED + "There are no teleport positions available for you!"),
     hub_created(ChatColor.GREEN + "Hub in world " + ChatColor.GOLD + "%s" + ChatColor.GREEN + " successfully created!"),
+    hub_disabled(ChatColor.RED + "The Hub is currently offline!"),
     available_points(ChatColor.GOLD + "Your available teleport positions:"),
     teleport_added(ChatColor.GREEN + "Teleport point " + ChatColor.GOLD + "%s" + ChatColor.GREEN + " added!"),
     teleport_already_exists(ChatColor.RED + "Teleport point " + ChatColor.GOLD + "%s" + ChatColor.RED + " already exists!"),
